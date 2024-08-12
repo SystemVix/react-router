a44ba9a6e268d8929387ea9286f92f0a53070fa5
Herhos <herhos@outlook.com>
2024-08-12 19:15:13 -0300

    Adicionando primeiros links que não recarregam a página inteira

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App()
@@ -5,6 +6,15 @@ function App()
   return (
     <>
       <h1>Bookkeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </>
   )
 }
