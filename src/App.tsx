f96b9e9818ae9e065750d352ce95816d3c4347aa
Herhos <herhos@outlook.com>
2024-08-13 08:46:46 -0300

    Rotas aninhadas ou sub-rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App()
@@ -15,6 +15,7 @@ function App()
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet/>
     </>
   )
 }
